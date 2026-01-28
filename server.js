d8f711a9f7c5bcd6f6b4e4777ce0b4e7e8e64e86
Sarada Prasanna Das <jkdas73@gmail.com>
2026-01-29 02:57:30 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const authRoutes = require("./routes/auth");
-const authMiddleware = require("./middleware/authMiddleware");
 const express = require("express");
 const cors = require("cors");
-const prisma = require("./prismaClient");
-const voteRoutes = require("./routes/vote");
 
+const authRoutes = require("./routes/auth");
+const voteRoutes = require("./routes/vote");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://gc-voting.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -19,6 +31,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api", voteRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
